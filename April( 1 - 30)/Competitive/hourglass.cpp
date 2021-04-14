7eea5fad76bfa11bfd35ca907e9ca987e0b1a31b
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2021-04-14 20:38:12 +0600

    mridul

---
+++ April( 1 - 30)/Competitive/hourglass.cpp
@@ -0,0 +1,68 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*     
+
+         ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+        ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+        ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+         ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+              ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+        ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+         ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define mp(a, b) make_pair(a, b)
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<pii>
+#define Mi map<int, int>
+#define mii map<pii, int>
+#define all(a) (a).begin(), (a).end()
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define endl '\n'
+
+#define F(i, s, e) for (ll i = s; i < e; ++i)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rem(i, a, b) for (int i = a; i > b; i--)
+#define P(str) cout << str << endl
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define mod 1000000007
+#define INF numeric_limits<ll>::max();
+#define NINF numeric_limits<ll>::min();
+const int N = int(1e5 + 3);
+
+int main()
+{
+    fast;
+    int ar[10][10],maxSum = -36985247;
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 6; j++)cin >> ar[i][j];
+    }
+
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 6; j++){
+            if(j+2 < 6 && i+2 < 6 ){
+                int sum = ar[i][j] + ar[i][j+1] + ar[i][j+2] + ar[i+1][j+1] + ar[i+2][j] + ar[i+2][j+1] + ar[i+2][j+2];
+                if(sum > maxSum)maxSum = sum;
+            }
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
+}
